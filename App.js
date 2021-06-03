12a875a8e413d224075030f1bc6c6fe1c718d180
saifulalamfahim <fahim12345saiful@gmail.comm>
2021-06-03 21:05:28 +0600

    commit

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Team 60 Is Here</Text>
+      <Image source={{ uri: "https://image.shutterstock.com/image-vector/abstract-futuristic-cyberspace-binary-code-260nw-740523562.jpg" }} style={{ width: 305, height: 159 }} />
       <StatusBar style="auto" />
     </View>
   );
