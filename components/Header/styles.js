e28c8829f313d1066ff8d85ff3851ceeb4e03480
zamanrabeen671 <manbeen671@gmail.com>
2021-06-04 15:55:11 +0600

    logo change

--- components/Header/styles.js
+++ components/Header/styles.js
@@ -11,8 +11,8 @@ const styles = StyleSheet.create({
     paddingHorizontal: 20,
   },
   logo: {
-    width: 100,
-    height: 20,
+    width: 150,
+    height: 30,
     resizeMode: 'contain',
   },
   menu: {
